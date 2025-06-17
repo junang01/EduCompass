330077a4d24dbbba83111948046b6ecff4095051
김소진 <153374849+kksssoozz@users.noreply.github.com>
2025-06-17 13:32:46 +0900

    Create book-rec.entity.ts

---
+++ backend/src/apis/book-rec/entities/book-rec.entity.ts
@@ -0,0 +1,52 @@
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Book } from '../../book/entities/book.entity';
+import { Subject } from '../../subject/entities/subject.entity';
+
+@ObjectType()
+@Entity()
+export class BookRecommendation {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => ID)
+  @Column()
+  userId: number;
+
+  @Field(() => User)
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Field(() => ID)
+  @Column()
+  bookId: number;
+
+  @Field(() => Book)
+  @ManyToOne(() => Book)
+  @JoinColumn({ name: 'bookId' })
+  book: Book;
+
+  @Field(() => ID)
+  @Column()
+  subject_seq: number;
+
+  @JoinColumn({ name: 'subject_seq' })
+  @Field(() => Subject)
+  @ManyToOne(() => Subject)
+  subject: Subject;
+
+  @Field()
+  @Column({ default: false })
+  isFavorite: boolean;
+
+  @Field()
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Field()
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
